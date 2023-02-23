7ef4b8ab9531f8b65cce7e60e2d3287274c7de40
fernando <fernandonetodeveloper@gmail.com>
2023-02-23 20:26:49 -0300

    stylesheet

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  return(
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+    }}>
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48,
+      }}>
+        Nome do evento
+      </Text>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16,
+      }}>
+        Sexta, 23 de Novembro de 2023
+      </Text>
+    </View>
+  )
+}
